65397ec35e928d139ef2ec155b828915739fc43f
krish08072006 <ssimarjeet898@gmail.comgit config --global user.name krish08072006git config --global user.email ssimarjeet898@gmail.com>
2025-11-01 00:39:00 +0530

    Added three new DSA Blind 75 solutions

---
+++ same_tree.py
@@ -0,0 +1,19 @@
+# Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+# Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        if not p and not q:
+            return True
+        if not p or not q or p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+    
+#   O(N)   TIME COMPLEXITY
